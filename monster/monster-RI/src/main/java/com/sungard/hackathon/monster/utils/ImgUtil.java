476443970925c03a44513c51928deef111670c41
Grisson.Wu <gangwu07+git@gmail.com>
2014-05-29 18:16:52 +0800

    add missed files

---
+++ monster/monster-RI/src/main/java/com/sungard/hackathon/monster/utils/ImgUtil.java
@@ -0,0 +1,91 @@
+package com.sungard.hackathon.monster.utils;
+
+import static org.bytedeco.javacpp.opencv_core.CV_32F;
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
+import static org.bytedeco.javacpp.opencv_core.cvCreateImage;
+import static org.bytedeco.javacpp.opencv_highgui.cvSaveImage;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_INTER_LINEAR;
+import static org.bytedeco.javacpp.opencv_imgproc.cvGetQuadrangleSubPix;
+import static org.bytedeco.javacpp.opencv_imgproc.cvResize;
+
+import java.util.logging.Logger;
+
+import org.bytedeco.javacpp.opencv_core.CvMat;
+import org.bytedeco.javacpp.opencv_core.CvSize;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+
+public class ImgUtil {
+
+	private static final Logger log = Logger.getLogger(ImgUtil.class.getName());
+
+	public static IplImage standardizeImage(final IplImage greyImage) {
+		log.info("image before standardiztion: " + greyImage);
+
+		int width = greyImage.width();
+		int height = greyImage.height();
+
+		IplImage resultImg = null;
+		if (width > height) {
+			resultImg = resize(rotateImage(greyImage));
+		} else {
+			resultImg = resize(greyImage);
+		}
+		log.info("image after standardiztion: " + resultImg);
+		return resultImg;
+	}
+
+	public static IplImage resize(final IplImage originImg) {
+		log.info("image before resize:" + originImg);
+
+		IplImage img = IplImage.create(Constants.IMG_WDITH, Constants.IMG_HEIGHT, IPL_DEPTH_8U, 1);
+		cvResize(originImg, img, CV_INTER_LINEAR);
+
+		log.info("image after resize:" + img);
+		return img;
+	}
+
+	public static void saveImage(IplImage img, String path) {
+		log.info("image: " + img + " to be saved at: " + path);
+		cvSaveImage(path, img);
+	}
+
+	public static IplImage rotateImage(final IplImage src) {
+		log.info("image before rotation: " + src);
+
+		float angleDegrees = -90f;
+		float angleRadians = angleDegrees * ((float) Math.PI / 180.0f);
+
+		CvMat M = CvMat.create(2, 3, CV_32F);
+		float[] m = new float[6];
+		int w = src.width();
+		int h = src.height();
+		m[0] = (float) (Math.cos(angleRadians));
+		m[1] = (float) (Math.sin(angleRadians));
+		m[3] = -m[1];
+		m[4] = m[0];
+		m[2] = w * 0.5f;
+		m[5] = h * 0.5f;
+		M.put(0, m[0]);
+		M.put(1, m[1]);
+		M.put(2, m[2]);
+		M.put(3, m[3]);
+		M.put(4, m[4]);
+		M.put(5, m[5]);
+
+		CvSize sizeRotated = new CvSize();
+		// sizeRotated.width(Math.round(w));
+		// sizeRotated.height(Math.round(h));
+		sizeRotated.width(Math.round(h));
+		sizeRotated.height(Math.round(w));
+
+		// Rotate
+		IplImage imageRotated = cvCreateImage(sizeRotated, src.depth(),
+				src.nChannels());
+
+		// Transform the image
+		cvGetQuadrangleSubPix(src, imageRotated, M);
+
+		log.info("image after rotation: " + src);
+		return imageRotated;
+	}
+}
