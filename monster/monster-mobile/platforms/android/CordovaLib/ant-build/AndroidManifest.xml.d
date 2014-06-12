09c3550b19cfd6c4e2fe66030d036e88bae6dbb4
Grisson.Wu <gangwu07+git@gmail.com>
2014-06-12 11:32:03 +0800

    add monster-mobile

---
+++ monster/monster-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Projects\Sungard\GIT\Monster_repo\monster\monster-mobile\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Projects\Sungard\GIT\Monster_repo\monster\monster-mobile\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ monster/monster-mobile/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Projects\Sungard\GIT\Monster_repo\monster\monster-mobile\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
